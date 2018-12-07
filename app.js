b0e2ec257fc52d9e83d80a3a03edd5cb7efbe739
junaid6141 <junaid2012@hotmail.co.uk>
2018-12-07 17:01:54 +0500

    Updating code

--- app.js
+++ app.js
@@ -1,67 +1,61 @@
-//This module help to listen request
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser.json({type: 'application/json'}));
-
-
-var toAddress = '';
-var fromAddress = '';
-var ContractAddress = '';
-var privateKey = '';
-var tokenValue = '';
-
+var Web3EthAccounts = require('web3-eth-accounts');
 
 const Web3 = require("web3");
 const web3 = new Web3();
 const Tx = require("ethereumjs-tx");
-var Web3EthAccounts = require('web3-eth-accounts');
 
-web3.setProvider(new web3.providers.HttpProvider("https://mainnet.infura.io/t2utzUdkSyp5DgSxasQX"));
+app.use(bodyParser.json({type: 'application/json'}));
+web3.setProvider(new web3.providers.HttpProvider("https://mainnet.infura.io/'Add your Infura Key'"));
 
-var abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"INITIAL_SUPPLY","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"wallet","type":"address"},{"name":"buyer","type":"address"},{"name":"tokenAmount","type":"uint256"}],"name":"pullBack","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"addr","type":"address"}],"name":"showMyTokenBalance","outputs":[{"name":"tokenBalance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wallet","type":"address"},{"name":"buyer","type":"address"},{"name":"tokenAmount","type":"uint256"}],"name":"mint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"wallet","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"message","type":"string"},{"indexed":false,"name":"addr","type":"address"},{"indexed":false,"name":"number","type":"uint256"}],"name":"Debug","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"}];
-var contractAddress = "0xaFBeC4D65BC7b116d85107FD05d912491029Bf46"; 
+var abi = "Add contract ABI without quotes";
+var contractAddress = "Add vontract address"; 
 var contract =  web3.eth.contract(abi).at(contractAddress);
+
+//------- Create Account ---------
 
 var createAccount = express.Router();
 createAccount.get('/', function(request, response){
-    // Enter your infura key
-    var account = new Web3EthAccounts('http://mainnet.infura.io/t2utzUdkSyp5DgSxasQX');
+    
+    let account = new Web3EthAccounts('http://mainnet.infura.io/"Add your Infura Key"');
 
     response.contentType('application/json');
     response.end(JSON.stringify(account.create()));
 });
 app.use('/create', createAccount);
 
+//------- Get Account Balance ---------
 
 var getBalance = express.Router();
 getBalance.post('/', function(request , response){
 
-    var toAddress = request.body.toAddress;
+    let toAddress = request.body.toAddress;
     response.contentType('application/json');
     response.end(JSON.stringify(contract.balanceOf(toAddress)));
 
 });
 app.use('/balance', getBalance);
 
+//------- Send Token ---------
 
 var sendToken = express.Router();
 sendToken.post('/', async function(request, response){
     
-    
-    fromAddress = request.body.from_address;
-    privateKey = request.body.from_private_key;
-    toAddress = request.body.to_address;
-    tokenValue = request.body.token_value;
+    let fromAddress = request.body.from_address;
+    let privateKey = request.body.from_private_key;
+    let toAddress = request.body.to_address;
+    let tokenValue = request.body.token_value;
 
     tokenValue = web3.toWei(tokenValue, 'ether');
 
     web3.eth.defaultAccount = fromAddress;
-    var count = web3.eth.getTransactionCount(web3.eth.defaultAccount);
-    var data = contract.transfer.getData(toAddress, tokenValue);
-    var gasPrice = web3.eth.gasPrice;
-    var gasLimit = 90000;
-    var rawTransaction = {
+    let count = web3.eth.getTransactionCount(web3.eth.defaultAccount);
+    let data = contract.transfer.getData(toAddress, tokenValue);
+    let gasPrice = web3.eth.gasPrice;
+    let gasLimit = 90000;
+    let rawTransaction = {
         "from": fromAddress,
         "nonce": web3.toHex(count),
         "gasPrice": web3.toHex(gasPrice),
@@ -70,11 +64,11 @@ sendToken.post('/', async function(request, response){
         "data": data,
         "chainId": 0x01
     };
-    var privKey = new Buffer(privateKey, 'hex');
-    var tx = new Tx(rawTransaction);
+    let privKey = new Buffer(privateKey, 'hex');
+    let tx = new Tx(rawTransaction);
 
     tx.sign(privKey);
-    var serializedTx = tx.serialize();
+    let serializedTx = tx.serialize();
 
     web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
         if (!err){
@@ -89,24 +83,23 @@ sendToken.post('/', async function(request, response){
 });
 app.use('/transferToken', sendToken);
 
-
+//------- Send Ether ---------
 
 var sendEther = express.Router();
 sendEther.post('/', async function(request, response){
-    
-    
-    fromAddress = request.body.from_address;
-    privateKey = request.body.from_private_key;
-    toAddress = request.body.to_address;
-    etherValue = request.body.ether_value;
+     
+    let fromAddress = request.body.from_address;
+    let privateKey = request.body.from_private_key;
+    let toAddress = request.body.to_address;
+    let etherValue = request.body.ether_value;
 
     etherValue = web3.toWei(etherValue, 'ether');
 
     web3.eth.defaultAccount = fromAddress;
-    var count = web3.eth.getTransactionCount(web3.eth.defaultAccount);
-    var gasPrice = web3.eth.gasPrice;
-    var gasLimit = 21000;
-    var rawTransaction = {
+    let count = web3.eth.getTransactionCount(web3.eth.defaultAccount);
+    let gasPrice = web3.eth.gasPrice;
+    let gasLimit = 21000;
+    let rawTransaction = {
         "from": fromAddress,
         "nonce": web3.toHex(count),
         "gasPrice": web3.toHex(gasPrice),
@@ -115,11 +108,11 @@ sendEther.post('/', async function(request, response){
         "value": web3.toHex(etherValue),
         "chainId": 0x01
     };
-    var privKey = new Buffer(privateKey, 'hex');
-    var tx = new Tx(rawTransaction);
+    let privKey = new Buffer(privateKey, 'hex');
+    let tx = new Tx(rawTransaction);
 
     tx.sign(privKey);
-    var serializedTx = tx.serialize();
+    let serializedTx = tx.serialize();
 
     web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
         if (!err){
@@ -140,8 +133,6 @@ app.get('/', function(request, response){
     response.contentType('application/json');
     response.end(JSON.stringify("Node is running"));
 });
-
-
 
 if (module === require.main) {
     // Start the server
